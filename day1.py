899da55cf4b4bf51c900a7f7ce4b04fc8f017c5c
Robin Michael <Siliconrob@siliconheaven.net>
2025-11-23 20:40:13 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -6,9 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def part1_solve(list1: list[int], list2: list[int]) -> int:
+    pass
+
+def part2_solve(list1: list[int], list2: list[int]) -> int:
+    pass
+
 def main() -> None:
     puzzle = Puzzle(year=2025, day=1)
     example = puzzle.examples.pop()
+    ic(part1_solve(example))
+    ic(part2_solve(example))
+
 
 
 if __name__ == '__main__':
