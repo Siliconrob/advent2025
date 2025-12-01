d9c109e3fac8ae42494c5f7096cf72aa53a9dfe9
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-01 01:01:56 -0600

    Copy day 1 to day 2

---
+++ day2.py
@@ -0,0 +1,64 @@
+from collections import Counter
+from typing import Tuple
+import more_itertools
+import networkx
+from aocd.models import Puzzle
+from icecream import ic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def parse_instruction(instruction: str) -> Tuple[int, int]:
+    direction = -1 if instruction[0:1] == 'L' else 1
+    return (
+        direction,
+        int(instruction[1:])
+    )
+
+def part1_solve(start_pos: int, instructions: list[str]) -> int:
+    zero_counts = 0
+    current_pos = start_pos
+    for instruction in instructions:
+        results = ic(parse_instruction(instruction))
+        current_pos = (current_pos + results[0] * results[1]) % 100
+        current_pos = abs(current_pos)
+        if current_pos == 0:
+            zero_counts += 1
+    return zero_counts
+
+def part2_solve(start_pos: int, instructions: list[str]) -> int:
+    zero_counts = 0
+    current_pos = start_pos
+    for instruction in instructions:
+        results = ic(parse_instruction(instruction))
+        move = results[0] * results[1]
+        if move + current_pos < 0 and current_pos != 0:
+            q,r = divmod(abs(move) + current_pos, 100)
+            if q == 0:
+                q = 1
+            zero_counts += q
+        if move + current_pos > 100:
+            q,r = divmod(move + current_pos, 100)
+            zero_counts += q
+        next_pos = (current_pos + move) % 100
+        current_pos = abs(next_pos)
+        if current_pos == 0:
+            zero_counts += 1
+    return zero_counts
+
+
+
+
+def main() -> None:
+    puzzle = Puzzle(year=2025, day=2)
+    example = puzzle.examples.pop()
+    example_data = example.input_data.splitlines()
+    start_position = 50
+    ic(part1_solve(start_position, example_data))
+    # ic(part1_solve(start_position, puzzle.input_data.splitlines()))
+
+    ic(part2_solve(start_position, example_data))
+    # ic(part2_solve(start_position, puzzle.input_data.splitlines()))
+
+if __name__ == '__main__':
+    main()
