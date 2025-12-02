54afa5789fd314653050b8c626013241982cb6c2
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-01 23:57:19 -0600

    Partial

--- day2.py
+++ day2.py
@@ -24,37 +24,41 @@ def part1_solve(id_ranges: list[str]) -> int:
             if is_palindrome(str(current_number)):
                 invalid_ids.append(current_number)
     return sum(invalid_ids)
-    # zero_counts = 0
-    # current_pos = start_pos
-    # for instruction in instructions:
-    #     results = ic(parse_instruction(instruction))
-    #     current_pos = (current_pos + results[0] * results[1]) % 100
-    #     current_pos = abs(current_pos)
-    #     if current_pos == 0:
-    #         zero_counts += 1
-    # return zero_counts
 
 
-def part2_solve(start_pos: int, instructions: list[str]) -> int:
-    zero_counts = 0
-    current_pos = start_pos
-    for instruction in instructions:
-        results = ic(parse_instruction(instruction))
-        move = results[0] * results[1]
-        if move + current_pos < 0 and current_pos != 0:
-            q, r = divmod(abs(move) + current_pos, 100)
-            if q == 0:
-                q = 1
-            zero_counts += q
-        if move + current_pos > 100:
-            q, r = divmod(move + current_pos, 100)
-            zero_counts += q
-        next_pos = (current_pos + move) % 100
-        current_pos = abs(next_pos)
-        if current_pos == 0:
-            zero_counts += 1
-    return zero_counts
+def find_longest_substring(input: str) -> str:
+    seen = []
+    for element in input:
+        if element in seen:
+            break
+        seen.append(element)
+    return "".join(seen)
 
+
+def part2_solve(id_ranges: list[str]) -> int:
+    invalid_ids = []
+    for id_range in id_ranges:
+        start, end = id_range.split('-')
+        for current_number in range(int(start), int(end)+1):
+            number_text = str(current_number)
+            counts = Counter(number_text)
+            if len(counts) == 1:
+                invalid_ids.append(current_number)
+                continue
+            if len(counts) < len(number_text):
+                for key, value in counts.items():
+                    if value > 1:
+                        number_text = number_text.replace(key, '', value)
+                if number_text == "":
+                    if is_palindrome(str(current_number)):
+                        invalid_ids.append(current_number)
+                        continue
+                    part = find_longest_substring(str(current_number))
+                    removed = str(current_number).replace(part, '')
+                    if removed == "":
+                        invalid_ids.append(current_number)
+
+    return sum(invalid_ids)
 
 def main() -> None:
     puzzle = Puzzle(year=2025, day=2)
@@ -63,11 +67,11 @@ def main() -> None:
     example_data = example.input_data.split(",")
 
 
-    #ic(part1_solve(example_data))
-    ic(part1_solve(puzzle.input_data.split(",")))
+    # ic(part1_solve(example_data))
+    # ic(part1_solve(puzzle.input_data.split(",")))
     #
-    # ic(part2_solve(start_position, example_data))
-    # ic(part2_solve(start_position, puzzle.input_data.splitlines()))
+    ic(part2_solve(example_data))
+    ic(part2_solve(puzzle.input_data.split(",")))
 
 if __name__ == '__main__':
     main()
