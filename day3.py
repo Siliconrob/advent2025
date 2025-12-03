9110d522cda71a35efaaa9c1efdc943d8c5d7d6c
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-03 00:47:56 -0600

    Part 2 done

--- day3.py
+++ day3.py
@@ -9,15 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def is_palindrome(input: str) -> bool:
-    mid_point = len(input) // 2
-    first_half = input[:mid_point]
-    second_half = input[mid_point:]
-    return first_half == second_half
-
-
 def part1_solve(batteries: list[str]) -> int:
-
     max_volts = []
     for battery in batteries:
         digits = [int(elem) for elem in list(battery)]
@@ -33,41 +25,24 @@ def part1_solve(batteries: list[str]) -> int:
 
 
 
-def find_longest_substring(input: str) -> str:
-    seen = []
-    for element in input:
-        if element in seen:
-            break
-        seen.append(element)
-    return "".join(seen)
+def part2_solve(batteries: list[str]) -> int:
+    max_volts = []
+    for battery in batteries:
+        digits = [int(elem) for elem in list(battery)]
 
+        while len(digits) > 12:
+            removed = False
+            for i in range(len(digits) - 1):
+                if digits[i] < digits[i + 1]:
+                    del digits[i]
+                    removed = True
+                    break
+            if not removed:
+                digits = digits[:-1]
 
-def part2_solve(id_ranges: list[str]) -> int:
-    invalid_ids = []
-    for id_range in id_ranges:
-        start, end = id_range.split('-')
-        for current_number in range(int(start), int(end)+1):
-            if current_number < 10:
-                continue
-            number_text = str(current_number)
-            counts = Counter(number_text)
-            if len(counts) == 1:
-                invalid_ids.append(current_number)
-                continue
-            if len(counts) < len(number_text):
-                for key, value in counts.items():
-                    if value > 1:
-                        number_text = number_text.replace(key, '', value)
-                if number_text == "":
-                    if is_palindrome(str(current_number)):
-                        invalid_ids.append(current_number)
-                        continue
-                    part = find_longest_substring(str(current_number))
-                    removed = str(current_number).replace(part, '')
-                    if removed == "":
-                        invalid_ids.append(current_number)
+        max_volts.append(int("".join(str(x) for x in digits)))
 
-    return sum(invalid_ids)
+    return sum(max_volts)
 
 def main() -> None:
     puzzle = Puzzle(year=2025, day=3)
@@ -77,9 +52,9 @@ def main() -> None:
 
     ic(part1_solve(example_data))
     ic(part1_solve(puzzle.input_data.splitlines()))
-    #
-    # ic(part2_solve(example_data))
-    # ic(part2_solve(puzzle.input_data.split(",")))
+
+    ic(part2_solve(example_data))
+    ic(part2_solve(puzzle.input_data.splitlines()))
 
 if __name__ == '__main__':
     main()
