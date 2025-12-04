d913548bd56352c36866fd7914191ab7bbb77e6d
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-04 00:09:42 -0600

    Day 4 Part 2 done

--- day5.py
+++ day5.py
@@ -83,16 +83,16 @@ def part2_solve(input_grid: list[str]) -> int:
 
 
 def main() -> None:
-    puzzle = Puzzle(year=2025, day=4)
+    puzzle = Puzzle(year=2025, day=5)
     data = puzzle.input_data
     example = puzzle.examples.pop()
     example_data = example.input_data.splitlines()
 
     ic(part1_solve(example_data))
-    ic(part1_solve(puzzle.input_data.splitlines()))
-
-    ic(part2_solve(example_data))
-    ic(part2_solve(puzzle.input_data.splitlines()))
+    # ic(part1_solve(puzzle.input_data.splitlines()))
+    #
+    # ic(part2_solve(example_data))
+    # ic(part2_solve(puzzle.input_data.splitlines()))
 
 
 if __name__ == '__main__':
