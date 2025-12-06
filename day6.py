779a1dab218afc653bc9fb059cd660b5bebc07d4
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-05 23:19:08 -0600

    Day 6 Part 1 done

--- day6.py
+++ day6.py
@@ -1,5 +1,8 @@
 from collections import Counter
+from operator import mul
 from typing import Tuple
+from urllib.response import addinfo
+
 import more_itertools
 import networkx
 from aocd.models import Puzzle
@@ -22,65 +25,50 @@ from dataclasses import dataclass, field
 load_dotenv()
 
 
-def parse_ranges(input_ranges: list[str]) -> list[Tuple[int, int]]:
-    ranges = []
-    for range_str in input_ranges:
-        start, end = range_str.split('-')
-        ranges.append((int(start), int(end)))
-    return ranges
+@dataclass
+class Problem:
+    index: int
+    numbers: list[int]
+    operation: str
 
+    def add(self, new_number: int) -> None:
+        self.numbers.append(new_number)
 
-def parse_ingredients(input_ids: list[str]) -> list[int]:
-    return [int(id) for id in input_ids]
-
-
-def part1_solve(input_ranges: list[str], input_ids: list[str]) -> int:
-    available_ranges = parse_ranges(input_ranges)
-    ingredients = parse_ingredients(input_ids)
-    fresh = []
-    for ingredient_id in ingredients:
-        for available_range in available_ranges:
-            start, end = available_range
-            if start <= ingredient_id <= end:
-                fresh.append(ingredient_id)
-                break
-    return len(fresh)
+def part1_solve(input_lines: list[str]) -> int:
+    operations = [z.strip() for z in input_lines.pop().split()]
+    problems = {}
+    for numbers in input_lines:
+        line_inputs = [int(z) for z in numbers.split()]
+        for index, number in enumerate(line_inputs):
+            current_problem = problems.get(index, None)
+            if current_problem is None:
+                problems[index] = Problem(index, [number], operations[index].strip())
+            else:
+                current_problem.add(number)
+    results = []
+    for key, value in problems.items():
+        if value.operation == "*":
+            results.append(reduce(mul, value.numbers))
+        else:
+            results.append(sum(value.numbers))
+    return sum(results)
 
 
 def part2_solve(input_ranges: list[str]) -> int:
-    available_ranges = parse_ranges(input_ranges)
-    available_ranges.sort(key=lambda z: z[0])
-    merged_ranges = [available_ranges[0]]
-    for current_range in available_ranges[1:]:
-        last_range = merged_ranges[-1]
-        if current_range[0] <= last_range[1] + 1:
-            merged_ranges[-1] = (last_range[0], max(last_range[1], current_range[1]))
-        else:
-            merged_ranges.append(current_range)
-    fresh_ingredients = 0
-    for current_range in merged_ranges:
-        items = current_range[1] - current_range[0] + 1
-        fresh_ingredients += items
-    return fresh_ingredients
+    pass
 
 
 def main() -> None:
     puzzle = Puzzle(year=2025, day=6)
     data = puzzle.input_data
     example = puzzle.examples.pop()
-    example_data = example.input_data.split("\n\n")
-    example_ingredient_ranges = example_data[0].splitlines()
-    example_ingredient_ids = example_data[1].splitlines()
+    example_data = example.input_data.splitlines()
 
-    puzzle_data = puzzle.input_data.split("\n\n")
-    ingredient_ranges = puzzle_data[0].splitlines()
-    ingredient_ids = puzzle_data[1].splitlines()
-
-    ic(part1_solve(example_ingredient_ranges, example_ingredient_ids))
-    ic(part1_solve(ingredient_ranges, ingredient_ids))
-
-    ic(part2_solve(example_ingredient_ranges))
-    ic(part2_solve(ingredient_ranges))
+    ic(part1_solve(example_data))
+    ic(part1_solve(puzzle.input_data.splitlines()))
+    #
+    # ic(part2_solve(example_data))
+    # ic(part2_solve(puzzle.input_data.splitlines()))
 
 
 if __name__ == '__main__':
