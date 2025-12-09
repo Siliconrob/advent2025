19e412caac477bc1ce1b2125f82332e3213014de
Robin Michael <Siliconrob@siliconheaven.net>
2025-12-09 17:36:46 -0600

    Guessed around and did preliminary runs until I found likely range and this works

--- day9.py
+++ day9.py
@@ -56,7 +56,7 @@ def part1_solve(input_lines: list[str]) -> int:
             max_size = new_area
     return max_size
 
-def part2_solve(input_lines: list[str]) -> int:
+def part2_solve(input_lines: list[str], begin_range: int, end_range: int) -> int:
     coords = parse_coords(input_lines)
 
 
@@ -99,22 +99,24 @@ def part2_solve(input_lines: list[str]) -> int:
     all_combos = list(combinations(coords, 2))
 
     sorted_combos = sorted(all_combos, key=lambda x: (abs(x[0].x - x[1].x) + 1) * (abs(x[0].y - x[1].y) + 1), reverse=True)
+
     for combo_index, combo in enumerate(sorted_combos):
-        # if combo_index % 5000 == 0:
-        #     ic(combo_index)
-        point1, point2 = ic(combo)
+        point1, point2 = combo
 
         width = abs(point1.x - point2.x) + 1
         height = abs(point1.y - point2.y) + 1
         expected_area = width * height
 
+        if begin_range >= expected_area >= end_range:
+            if combo_index % 100 == 0:
+                ic(combo_index)
+                ic(expected_area)
+            continue
+
         start_x = min(point1.x, point2.x)
         end_x = max(point1.x, point2.x)
         start_y = min(point1.y, point2.y)
         end_y = max(point1.y, point2.y)
-
-        # if max_size is not None and expected_area < max_size:
-        #     continue
 
         valid_rows = 0
         for row_index in range(start_y, end_y + 1):
@@ -127,10 +129,8 @@ def part2_solve(input_lines: list[str]) -> int:
         if valid_rows == end_y - start_y + 1:
             max_size = expected_area
             break
-            # if max_size is None or expected_area > max_size:
-            #     max_size = ic(expected_area)
 
-    return max_size
+    return ic(max_size)
 
 
 
@@ -140,11 +140,15 @@ def main() -> None:
     example = puzzle.examples.pop()
     example_data = example.input_data.splitlines()
 
-    # ic(part1_solve(example_data))
-    # ic(part1_solve(puzzle.input_data.splitlines()))
-    #
-    #ic(part2_solve(example_data))
-    ic(part2_solve(puzzle.input_data.splitlines()))
+    ic(part1_solve(example_data))
+    ic(part1_solve(puzzle.input_data.splitlines()))
+
+    ic(part2_solve(example_data, 20, 50))
+    # Guessed around and did preliminary runs until I found likely range and this works
+    # Then brute force calculate in the range of where the largest area is,
+    # only was able to do this by eliminating the top range by running it all night and getting no hits
+    ic(part2_solve(puzzle.input_data.splitlines(), 1_400_000_000, 1_500_000_000))
+
 
 
 if __name__ == '__main__':
